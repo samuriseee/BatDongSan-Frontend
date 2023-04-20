e35840559d6431f6669713d40fa181f3edda1bc7
hung <anhkp222@gmail.com>
2023-04-21 00:32:38 +0700

    update estateDetail

--- src/views/EstateDetails.vue
+++ src/views/EstateDetails.vue
@@ -2,17 +2,21 @@
     <div class="container">
         <SideBar />
         <div class="create-post">
-            hehehe 
+            <SearchBarOnEstateList />
+            <EstateInfoSessionVue />
         </div>
     </div>
 </template>
 
 <script>
+import SearchBarOnEstateList from '@/components/ui/SearchBarOnEstateList.vue'
+import EstateInfoSessionVue from '../components/EstateDetailPage/EstateInfoSession.vue'
 export default {
-
+    components: {
+        SearchBarOnEstateList,
+        EstateInfoSessionVue,
+    },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
