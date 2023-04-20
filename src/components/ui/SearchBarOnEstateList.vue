3c2bd6eb9e4fa8b3d74fd0ce060a00eb0501dfd2
CaoTuanKiett <tuankiet06032002@gmail.com>
2023-04-21 00:51:41 +0700

    'update_searchBar'

--- src/components/ui/SearchBarOnEstateList.vue
+++ src/components/ui/SearchBarOnEstateList.vue
@@ -5,19 +5,22 @@
       <router-link to="/estate-for-sale"> Bán </router-link>
     </div>
     <div>
-      <input type="text" class="search-box" />
+      <input style="color: #ccc;" type="text" class="search-box"  value="Dự án hehe"/>
     </div>
-    <div class="relative inline-block text-left">
-      <div>
+    <div class="list__option relative text-left flex">
+      <div >
         <button
           type="button"
-          class="inline-flex justify-center w-full shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+          class="item__option inline-flex justify-center w-full shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
           id="options-menu"
           aria-haspopup="true"
           aria-expanded="true"
           @click="openFilter"
         >
-          Loại nhà đất
+          <div class="option_input">
+            <p>Loại nhà đất</p>
+          <span>Tất cả</span>
+          </div>
           <!-- Heroicon name: chevron-down -->
           <svg
             class="-mr-1 ml-2 h-5 w-5"
@@ -34,8 +37,158 @@
           </svg>
         </button>
       </div>
+
+      <div>
+        <button
+          type="button"
+          class="item__option inline-flex justify-center w-full shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+          id="options-menu"
+          aria-haspopup="true"
+          aria-expanded="true"
+          @click="openFilter"
+        >
+          <div class="option_input">
+            <p>Khu vực & Dự án</p>
+          <span>Toàn quốc</span>
+          </div>
+          <!-- Heroicon name: chevron-down -->
+          <svg
+            class="-mr-1 ml-2 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6.293 7.707a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 011.414 0l.707.707a1 1 0 010 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414l.707-.707z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div>
+        <button
+          type="button"
+          class=" item__option inline-flex justify-center w-full shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+          id="options-menu"
+          aria-haspopup="true"
+          aria-expanded="true"
+          @click="openFilter"
+        >
+          <div class="option_input">
+            <p>Mức giá</p>
+          <span>Tất cả</span>
+          </div>
+          <!-- Heroicon name: chevron-down -->
+          <svg
+            class="-mr-1 ml-2 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6.293 7.707a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 011.414 0l.707.707a1 1 0 010 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414l.707-.707z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div>
+        <button
+          type="button"
+          class=" item__option inline-flex justify-center w-full shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+          id="options-menu"
+          aria-haspopup="true"
+          aria-expanded="true"
+          @click="openFilter"
+        >
+          <div class="option_input">
+            <p>Diện tích</p>
+          <span>Tất cả</span>
+          </div>
+          <!-- Heroicon name: chevron-down -->
+          <svg
+            class="-mr-1 ml-2 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6.293 7.707a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 011.414 0l.707.707a1 1 0 010 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414l.707-.707z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div>
+        <button
+          type="button"
+          class="item__option btn-option inline-flex justify-center w-full shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+          id="options-menu"
+          aria-haspopup="true"
+          aria-expanded="true"
+          @click="openFilter"
+        >
+          <div class="option_input">
+            <span>Lọc Thêm</span>
+          </div>
+          <!-- Heroicon name: chevron-down -->
+          <svg
+            class="-mr-1 ml-2 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6.293 7.707a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 011.414 0l.707.707a1 1 0 010 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414l.707-.707z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div>
+        <button
+          type="button"
+          class="item__option inline-flex justify-center w-full shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+          id="options-menu"
+          aria-haspopup="true"
+          aria-expanded="true"
+          @click="openFilter"
+        >
+          <div class="option_input">
+          <span>Đặt lại</span>
+          </div>
+          <!-- Heroicon name: chevron-down -->
+          <svg
+            class="-mr-1 ml-2 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6.293 7.707a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 011.414 0l.707.707a1 1 0 010 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414l.707-.707z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+
+      
       <div
-        class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+        class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-white ring-opacity-5"
         aria-labelledby="options-menu"
         role="menu"
       >
@@ -201,4 +354,34 @@ input[type="checkbox"] {
   align-items: center;
   border-top: 1px solid #f2f2f2;
 }
+
+
+
+
+
+// -----------------------------
+.item__option {
+  padding-right: 40px;
+  align-items: center;
+  height: 100%;
+}
+
+.btn-option svg {
+  font-size: 14px;
+  color: #ccc;
+  margin-left: 10px;
+}
+
+.option_input {
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+}
+
+.option_input span {
+  font-size: 14px;
+  font-weight: 600;
+}
+
+
 </style>
