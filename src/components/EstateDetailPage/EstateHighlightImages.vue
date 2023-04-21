eee13e9911b8eb425612857da1dbb7e0435f2c2c
CaoTuanKiett <tuankiet06032002@gmail.com>
2023-04-21 13:37:41 +0700

    'updateUI_Detail'

---
+++ src/components/EstateDetailPage/EstateHighlightImages.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="highlight-images">
+        <div class="image-large">
+            <img :src="images[count]" alt="" />
+            <div
+                class="button-action-left"
+                :class="{ hidden: this.count == 0 }"
+                @click="$emit('prev-image')"
+            >
+                L
+            </div>
+            <div
+                class="button-action-right"
+                :class="{ hidden: this.count == images.length - 1 }"
+                @click="$emit('next-image')"
+            >
+                R
+            </div>
+            <div class="current-page">1/11</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EstateHighlightImages',
+    props: {
+        images: {
+            type: Array,
+            required: true,
+        },
+        count: {
+            type: Number,
+            required: true,
+        },
+    },
+    methods: {
+        nextImage() {
+            this.$emit('nextImage')
+        },
+        prevImage() {
+            this.$emit('prevImage')
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.highlight-images {
+    .image-large {
+        position: relative;
+        img {
+            width: 100%;
+        }
+        .button-action-left,
+        .button-action-right {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: absolute;
+            top: 50%;
+            width: 30px;
+            height: 30px;
+            background-color: white;
+            transform: translateY(-50%);
+            border-radius: 4px;
+            border: solid 1px #ccc;
+            &.hidden {
+                display: none;
+            }
+        }
+        .button-action-left {
+            left: 16px;
+        }
+        .button-action-right {
+            right: 16px;
+        }
+    }
+}
+</style>
---
+++ src/components/EstateDetailPage/EstateInfoSession.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="session-estate-info">
+            <div class="content-left">
+                <estate-highlight-images
+                    :images="images"
+                    :count="count"
+                    @next-image="nextImage"
+                    @prev-image="prevImage"
+                />
+            </div>
+            <div class="content-righy"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import EstateHighlightImages from './EstateHighlightImages.vue'
+export default {
+    components: { EstateHighlightImages },
+    name: 'EstateInfoSession',
+    comments: {
+        EstateHighlightImages,
+    },
+    data() {
+        return {
+            images: [
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+            ],
+            count: 0,
+        }
+    },
+    methods: {
+        nextImage() {
+            this.count += 1
+            console.log(this.count)
+        },
+        prevImage() {
+            this.count -= 1
+            console.log(this.count)
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    max-width: 936px;
+    margin: 24px auto 0 auto;
+    background: black;
+}
+</style>
